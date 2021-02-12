4abd7bc55041e3fa85c78837290a6ef45c96fd22
arosh1909 <arushverma903@gmail.com>
2021-02-12 09:45:53 +0530

    done till 17.2

--- sketch.js
+++ sketch.js
@@ -1,44 +1,39 @@
-function preload(){
-  sound = loadSound('file.wav');
-}
+var sound;
+var sliderRate;
+var sliderPan;
+var sliderValue;
+var button;
 
 function setup(){
-  let cnv = createCanvas(100,100);
-  cnv.mouseClicked(togglePlay);
-  fft = new p5.FFT();
-  sound.amp(0.2);
+  createCanvas(200,200);
+  sound = loadSound("file3.mp3", loaded); // Callback
+  // sound.setVolume(0.5);
+  // sliderRate = createSlider(0, 1.5, 1, 0.01)
+  // sliderPan = createSlider(-1, 1, 0, 0.01)
+  button = createButton("Play");
+  button.mousePressed(togglePlaying);
+}
+
+function loaded(){
+  //sound.play();
+}
+
+function togglePlaying(){
+  if(!sound.isPlaying()){
+    sound.play();
+    sound.setVolume(0.5);
+    button.html("pause"); 
+  }else{
+    sound.pause();
+    button.html("play");
+  }
+  // sound.play();
 }
 
 function draw(){
   background(220);
-
-  let spectrum = fft.analyze();
-  noStroke();
-  fill(255, 0, 255);
-  for (let i = 0; i< spectrum.length; i++){
-    let x = map(i, 0, spectrum.length, 0, width);
-    let h = -height + map(spectrum[i], 0, 255, height, 0);
-    rect(x, height, width / spectrum.length, h )
-  }
-
-  let waveform = fft.waveform();
-  noFill();
-  beginShape();
-  stroke(20);
-  for (let i = 0; i < waveform.length; i++){
-    let x = map(i, 0, waveform.length, 0, width);
-    let y = map( waveform[i], -1, 1, 0, height);
-    vertex(x,y);
-  }
-  endShape();
-
-  text('tap to play', 20, 20);
+  // sound.setVolume(slider.value()); 
+  // sound.rate(sliderRate.value());
+  // sound.pan(sliderPan.value());
 }
 
-function togglePlay() {
-  if (sound.isPlaying()) {
-    sound.pause();
-  } else {
-    sound.loop();
-  }
-}
